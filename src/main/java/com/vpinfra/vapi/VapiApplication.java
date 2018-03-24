14e4ee8e94a2ce3d4fc3373e47107a44c097fb86
junfeng <junfeng@192.168.0.104>
2018-03-24 22:33:15 +0800

    初始化提交

---
+++ src/main/java/com/vpinfra/vapi/VapiApplication.java
@@ -0,0 +1,12 @@
+package com.vpinfra.vapi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class VapiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(VapiApplication.class, args);
+	}
+}
---
+++ src/main/java/com/vpinfra/vapi/controller/IndexController.java
@@ -0,0 +1,18 @@
+package com.vpinfra.vapi.controller;
+
+import javax.servlet.http.HttpServletRequest;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+@RequestMapping()
+public class IndexController {
+
+	@GetMapping("/index")
+	public String index(HttpServletRequest request) {
+		request.setAttribute("username", "张三");
+		return "index";
+	}
+}
