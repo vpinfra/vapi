b8ff88ea2c4a169643d91c0c30c90d04e44fc2cf
harveyspace <harveyer@163.com>
2018-07-23 23:00:17 +0800

    init module

---
+++ vcore/src/main/java/com/vpinfra/core/VcoreApplication.java
@@ -0,0 +1,12 @@
+package com.vpinfra.core;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class VcoreApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(VcoreApplication.class, args);
+    }
+}
---
+++ vsso/src/main/java/com/vpinfra/sso/VssoApplication.java
@@ -0,0 +1,12 @@
+package com.vpinfra.sso;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class VssoApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(VssoApplication.class, args);
+    }
+}
